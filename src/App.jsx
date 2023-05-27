79e919dc5f8426f2ae7d56f078e208e5e1b39c8b
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-27 16:16:57 +0530

    adding all the required components for the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar';
+import Banner from "./components/banner/Banner.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Features from "./components/features/Features.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Resume from "./components/resume/Resume.jsx";
+import Testimonial from "./components/testimonial/Testimonials.jsx";
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+        <Navbar />
+      <div className="max-w-screen-xl mx-auto">
+        <Banner />
+        <Features />
+        <Projects />
+        <Resume />
+        <Testimonial />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
